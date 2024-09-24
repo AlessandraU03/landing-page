02c5ac41ee4263f8e3ff064af288f67a89d2dc71
AlessandraU03 <233303@ids.upchiapas.edu.mx>
2024-09-24 10:31:57 -0600

    Add:Componentes organizados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { DashboardMovieComponent } from './movie/dashboard-movie/dashboard-movie.component';
+import { HeaderMovieComponent } from './movie/header-movie/header-movie.component';
+import { ContentMovieComponent } from './movie/content-movie/content-movie.component';
+import { FooterMovieComponent } from './movie/footer-movie/footer-movie.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardMovieComponent
+    DashboardMovieComponent,
+    HeaderMovieComponent,
+    ContentMovieComponent,
+    FooterMovieComponent,
   ],
   imports: [
     BrowserModule
